0f6a59f0a0c2efc3e2b6a566d4fe4deb25cb7042
natalie sinner <nataliesinner@hotmail.com>
2020-04-09 22:07:10 -0700

    added error 500

--- server.js
+++ server.js
@@ -13,35 +13,26 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(cors());
 
-// Test Endpoint
-// http://localhost:3000/test
-app.get( '/test', (request, response) => {
-  const name = request.query.name;
-  response.send( `Hello, ${name}` );
-});
-
-app.get('/cats', (request, response) => {
-  let type = request.query.type;
-  let words = '';
-  if ( type === 'calico' ) {
-    words = 'You are a good person';
-  }
-  else {
-    words = 'We do not have those';
-  }
-
-  response.send(words);
-});
 
 app.get('/location', handleLocation);
 
 function handleLocation( request, response ) {
-  let city = request.query.city;
-  // eventually, get this from a real live API
-  // But today, pull it from a file.
-  let locationData = require('./data/geo.json');
-  let location = new Location(city, locationData[0]);
-  response.json(location);
+  try {
+    let city = request.query.city;
+    // eventually, get this from a real live API
+    // But today, pull it from a file.
+    let locationData = require('./data/geo.json');
+    let location = new Location(city, locationData[0]);
+    // throw 'john is ugly or something';
+    response.json(location);
+  }
+  catch(error) {
+    let errorObject = {
+      status: 500,
+      responseText: 'john is ugly or something',
+    };
+    response.status(500).json(errorObject);
+  }
 }
 
 function Location(city, data) {
@@ -51,37 +42,35 @@ function Location(city, data) {
   this.longitude = data.lon;
 }
 
-/* Restaurants
 
-  {
-    "restaurant": "Serious Pie",
-    "cuisines": "Pizza, Italian",
-    "locality": "Belltown"
-  },
-*/
+app.get('/weather', handleWeather);
 
-app.get('/restaurants', handleRestaurants);
-
-function handleRestaurants(request, response) {
-  // Eventually, will be an API call
-  // Today ... get a file
-
-  let restaurantData = require('./data/restaurants.json');
-  let listOfRestaurants = [];
-
-  restaurantData.nearby_restaurants.forEach( r => {
-    let restaurant = new Restaurant(r);
-    listOfRestaurants.push(restaurant);
-  });
-
-  response.json(listOfRestaurants);
-
+function handleWeather(request, response) {
+  try {
+  // use darksky fake data
+  // eventually will be an api call
+    let weatherData = require('./data/darksky.json');
+    let listofDays = [];
+    weatherData.daily.data.forEach( day => {
+      let weather = new Weather(day);
+      listofDays.push(weather);
+    })
+    response.json(listofDays);
+  }
+  catch(error) {
+    let errorObject = {
+      status: 500,
+      responseText: 'john is ugly or something',
+    };
+    response.status(500).json(errorObject);
+  }
 }
 
-function Restaurant(data) {
-  this.name = data.restaurant.name;
-  this.cuisines = data.restaurant.cuisines;
-  this.locality = data.restaurant.location.locality;
+function Weather(data) {
+  this.time = data.time;
+  this.forecast = data.summary;
 }
+
+
 
 app.listen( PORT, () => console.log('Server up on', PORT));
